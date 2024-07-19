667bc7826dea764d134d08be6a6c57b38747b4cb
stringtheory92 <adamkabak@gmail.com>
2024-07-19 07:53:36 -0400

    openai and llm studio integ

--- src/crew_2/main.py
+++ src/crew_2/main.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 import sys
 from crew_2.crew import Crew2Crew
+# from dotenv import load_dotenv
+import os
 
+
+# load_dotenv()
 
 def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
         'topic': 'AI LLMs'
     }
+    # print(os.getenv("OPENAI_API_BASE"))
+    # print(os.getenv("OPENAI_API_KEY"))
+
     Crew2Crew().crew().kickoff(inputs=inputs)
 
 
